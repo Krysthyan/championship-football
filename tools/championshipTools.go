2e137706ad46839c770bb214c4f9f31cbf87d862
krysthyan <krysthyan_09h@yahoo.com>
2017-01-19 23:43:15 -0500

    agreado, para obtener token (size = 5) para los id's

--- tools/championshipTools.go
+++ tools/championshipTools.go
@@ -1,1 +1,13 @@
 package tools
+
+import (
+	"github.com/nkanish2002/token_generator"
+	"strings"
+)
+
+func ChampionshipToken(size int) string {
+	g := token_generator.Generator{}
+	g.New()
+
+	return strings.ToUpper(g.GetToken(size))
+}

cd483e2587647219ff581ecfb7c0219790c2c5c8
krysthyan <krysthyan_09h@yahoo.com>
2017-01-20 03:35:55 -0500

    refactor

--- public/app/app-routing.module.ts
+++ public/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import {NgModule}             from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {TeamDesktopComponent}   from './team/team_desktop/team_desktop.component';
-import {TeamComponent} from './team/team_description/team_description.component'
+
 
 const routes: Routes = [
     {path: '', redirectTo: '/desktop', pathMatch: 'full'},
-    {path: 'desktop', component: TeamDesktopComponent},
-    {path: 'team', component: TeamComponent}
+    {path: 'desktop', component: TeamDesktopComponent}
+
 ];
 
 @NgModule({

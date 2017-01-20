7ad90e0a56452d56ab5e2276e8f72bef88afc7c3
krysthyan <krysthyan_09h@yahoo.com>
2017-01-19 23:42:16 -0500

    guardar equipo router

--- routers/router.team.go
+++ routers/router.team.go
@@ -1,1 +1,20 @@
 package routers
+
+import (
+	"github.com/gorilla/mux"
+	"championship-football/controllers"
+)
+
+func RouterTeam(championship *mux.Router)  {
+
+	championship.HandleFunc(
+		"/team/list",
+		controllers.GetListTeam,
+	).Methods("GET")
+
+	championship.HandleFunc(
+		"/team/save",
+		controllers.SaveTeam,
+	).Methods("POST")
+
+}

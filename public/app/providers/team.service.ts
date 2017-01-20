ffcb653d421adcfa1f61dbd891bfa932c500e9ad
krysthyan <krysthyan_09h@yahoo.com>
2017-01-19 23:45:10 -0500

    agregado el metodo post para guardar un equipo

--- public/app/providers/team.service.ts
+++ public/app/providers/team.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Headers, RequestOptions} from "@angular/http";
 
 @Injectable()
 export class HeroService {
@@ -8,13 +8,23 @@ export class HeroService {
     }
 
     getTeam() {
-        let json = this.http.get("http://localhost:8080/getTeam");
+        let json = this.http.get("http://localhost:8080/team/list");
         return json;
     }
 
     getPersonTeam(id: number){
         let json = this.http.get("http://localhost:8080/getPersonTeam?id="+ id );
         return json;
+    }
+
+    postSaveTeam(team: any){
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        let body = JSON.stringify(team);
+        console.log(body);
+        let json = this.http.post('http://localhost:8080/team/save', body, headers);
+        return json
+
     }
 
 

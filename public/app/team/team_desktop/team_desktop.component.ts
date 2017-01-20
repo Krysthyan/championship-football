b5a14d904aa1ddd532e2303cc5f862724f818fa8
krysthyan <krysthyan_09h@yahoo.com>
2017-01-19 23:44:27 -0500

    implemetado el modal para guardar un equipo

--- public/app/team/team_desktop/team_desktop.component.ts
+++ public/app/team/team_desktop/team_desktop.component.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
-
+import {HeroService} from '../../providers/team.service'
 
 @Component({
     moduleId: module.id,
     selector: 'my-team',
-    templateUrl: 'team_desktop.component.html'
+    templateUrl: 'team_desktop.component.html',
+    providers: [HeroService]
+
 })
 
 export class TeamDesktopComponent {
@@ -12,7 +14,26 @@ export class TeamDesktopComponent {
     public id_team_select = 0;
     public name_team_select = '';
 
-    constructor() {
+    public name_save_team = '';
+
+    public teams_save_return = {};
+
+    constructor(private hero: HeroService) {
+    }
+
+    saveTeam(){
+        console.log({name: this.name_save_team});
+        this.hero.postSaveTeam({name: this.name_save_team}).subscribe(
+            data => {
+                this.teams_save_return = data.json();
+            },
+            err => {
+                console.log(err)
+            },
+            () => {
+                console.log("team save")
+            }
+        );
     }
 
 }

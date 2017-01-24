89988553b7337cdcc07879383b0d99206571b966
krysthyan <krysthyan_09h@yahoo.com>
2017-01-24 00:11:07 -0500

    add RouterTeam and RouterChampionship

--- routers/router.go
+++ routers/router.go
@@ -1,26 +1,14 @@
 package routers
 
 import (
-	"championship-football/controllers"
+	//"championship-football/controllers"
 	"github.com/gorilla/mux"
 )
 
-func InitRouter(championship *mux.Router) *mux.Router {
+func InitRouter(championship *mux.Router)  {
 
-	championship.HandleFunc(
-		"/getTeam",
-		controllers.GetTeam,
-	).Methods("GET")
+	RouterTeam(championship)
+	RouterPerson(championship)
+	RouterChampionship(championship)
 
-	championship.HandleFunc(
-		"/getPerson",
-		controllers.GetPerson,
-	).Methods("GET")
-
-	championship.HandleFunc(
-		"/getPersonTeam",
-		controllers.GetPersonTeam,
-	).Methods("GET")
-
-	return championship
 }

def8585cd756a75bc075fec9ccf1fcbab269ae18
krysthyan <krysthyan_09h@yahoo.com>
2017-01-24 00:56:46 -0500

    refactor

--- controllers/controller.championship.go
+++ controllers/controller.championship.go
@@ -24,25 +24,32 @@ func InsertChampionship(w http.ResponseWriter, h *http.Request) {
 
 func GetChampionship(w http.ResponseWriter, h *http.Request)  {
 	LogChampionship("GET", "championship/get", strconv.Itoa(http.StatusOK))
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+
 	w.Write(models.GetChampionship(h.URL.Query().Get("id")))
 }
 
 func GetChampionshipList(w http.ResponseWriter, h *http.Request)  {
 	LogChampionship("GET", "championship/getList", strconv.Itoa(http.StatusOK))
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+
 	w.Write(models.GetChampionshipList())
 }
+
 func DeleteChampionship(w http.ResponseWriter, h *http.Request)  {
 	LogChampionship("DELETE", "championship/delete", strconv.Itoa(http.StatusOK))
+
 	var championship models.Championship
 	err := json.NewDecoder(h.Body).Decode(&championship)
 
 	if err != nil {
 		log.Println(err)
 	}
+
 	models.DeleteChampionship(championship)
 
 	w.Header().Set("Content-Type", "application/json")

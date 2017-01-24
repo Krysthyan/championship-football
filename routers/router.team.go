7abae9124cc2c8617ef1be6422c2fa5a165cc9e8
krysthyan <krysthyan_09h@yahoo.com>
2017-01-24 00:57:13 -0500

    refactor

--- routers/router.team.go
+++ routers/router.team.go
@@ -10,7 +10,7 @@ func RouterTeam(championship *mux.Router)  {
 
 	championship.HandleFunc(
 		"/team/list",
-		controllers.GetListTeam,
+		controllers.GetTeamList,
 	).Methods("GET")
 
 	championship.HandleFunc(
@@ -23,15 +23,6 @@ func RouterTeam(championship *mux.Router)  {
 		controllers.GetTeamsFromChampionship,
 	).Methods("GET")
 
-	/*championship.HandleFunc(
-		"/team/modify",
-		controllers.ModifyTeam,
-	).Methods("UPDATE")
-
-	championship.HandleFunc(
-		"/team/get",
-		controllers.GetTeam,
-	).Methods("GET")*/
 
 
 

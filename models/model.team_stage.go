ad5ab65377ecf8b3d952312170ddbc4468e3cc9d
krysthyan <krysthyan_09h@yahoo.com>
2017-01-28 02:01:19 -0500

    modelo Team_stage

--- models/model.team_stage.go
+++ models/model.team_stage.go
@@ -1,5 +1,16 @@
 package models
 
+type Team_stage struct {
+	Team_id string `json:"team_id" orm:"pk"`
+	Stage_id string `json:"stage_id"`
+	Championship_id string `json:"championship_id"`
+}
+
+func InsertTeamStage(teamStage Team_stage) []byte {
+	return ORM_INSERT(teamStage)
+}
+
+
 func IsChampionshipInTable(id string) (is_exist bool) {
 
 	count, _ := ORM().QueryTable("stage_team").Filter("championship_id", id).Count()

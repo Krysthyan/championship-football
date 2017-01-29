6200bf7460c907245e22fd3a99cd520893cf6665
krysthyan <krysthyan_09h@yahoo.com>
2017-01-28 23:48:38 -0500

    implementacion de partidos

--- models/model.match.go
+++ models/model.match.go
@@ -1,1 +1,33 @@
 package models
+
+
+type Match struct {
+	Id string `json:"id" orm:"pk"`
+	Team_winner string `json:"team_winner" orm:"null;on_update(set_null)"`
+	Team_losser string `json:"team_losser" orm:"null;on_update(set_null)"`
+	Stage_id string `json:"stage_id"`
+}
+
+type Combinations struct {
+	team1 Team
+	team2 Team
+}
+
+func InsertMatch(match Match) []byte {
+	return ORM_INSERT(match)
+}
+
+func CombinationsTeam(iterable []Team) (partidos []Combinations)  {
+
+	for index:=len(iterable)-1;index > 0; index-- {
+		for index1:=index-1;index1 >= 0; index1-- {
+
+			partidos = append(partidos, Combinations{
+				team1:iterable[index],
+				team2:iterable[index1],
+			})
+
+		}
+	}
+	return
+}

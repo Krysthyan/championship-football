2c888a4c69291fc6556eff3485bbee812d57f091
krysthyan <krysthyan_09h@yahoo.com>
2017-01-28 23:01:22 -0500

    metodo para obtener los equipos de un grupo

--- models/model.team_stage.go
+++ models/model.team_stage.go
@@ -1,15 +1,45 @@
 package models
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type Team_stage struct {
 	Team_id string `json:"team_id" orm:"pk"`
 	Stage_id string `json:"stage_id"`
-	Championship_id string `json:"championship_id"`
 }
 
 func InsertTeamStage(teamStage Team_stage) []byte {
 	return ORM_INSERT(teamStage)
 }
 
+func GetLevelChampionship(Championship_id string) (level int) {
+	ORM().Raw("CALL getRaundHigher(?)", Championship_id).QueryRow(&level)
+
+	return
+}
+
+func GetTeamFromStage(Stage_id string) (mapB []byte) {
+	var team_stage []Team_stage
+	var listTeam_stage ListTeamStage
+
+	ORM().QueryTable("team_stage").Filter("stage_id",  Stage_id).All(&team_stage)
+	var teams []Team
+	for _, element := range team_stage{
+
+		var team Team
+		json.Unmarshal(GetTeam(element.Team_id),&team)
+		teams = append(teams, team)
+	}
+	log.Println(teams)
+	listTeam_stage = ListTeamStage{
+		Id:team_stage[0].Stage_id,
+		Team:teams,
+	}
+	mapB, _ = json.Marshal(listTeam_stage)
+	return
+}
 
 func IsChampionshipInTable(id string) (is_exist bool) {
 

7f63b08020561d08556a87dd00ef8bd0ef205f4b
krysthyan <krysthyan_09h@yahoo.com>
2017-01-28 22:59:52 -0500

    metodo updateStage, metodo para jugar los grupos y obtener la lista de stages

--- models/model.stage.go
+++ models/model.stage.go
@@ -7,12 +7,14 @@ import (
 	"time"
 	"math/rand"
 	"errors"
+	"log"
 )
 
 type Stage struct {
 	Id string `json:"id" orm:"pk"`
 	Round int `json:"round"`
-	IsStage bool `json:"is_stage"`
+	IsStage int `json:"is_stage"`
+	Championship_id string `json:"championship_id"`
 }
 
 type ListTeamStage struct {
@@ -22,6 +24,17 @@ type ListTeamStage struct {
 
 func InsertStage(stage Stage) []byte {
 	return ORM_INSERT(stage)
+}
+
+func UpdateStage(Stage_id string, ImcrementRound int)  {
+	stage := Stage{Id: Stage_id}
+	if ORM().Read(&stage) == nil {
+		stage.Round += ImcrementRound
+
+		if _, err := ORM().Update(&stage); err == nil {
+			log.Println(err)
+		}
+	}
 }
 
 func GenerateFaseGrupos(teams []Team, championship_id string) (mapB []byte) {
@@ -49,12 +62,11 @@ func GenerateFaseGrupos(teams []Team, championship_id string) (mapB []byte) {
 					teams_alt = nil
 				}
 
-				json.Unmarshal(InsertStage(GetNewStage()),&stage)
+				json.Unmarshal(InsertStage(GetNewStage(championship_id)),&stage)
 				isNewGoup = 0
 			}
 			team := teams[ramdon]
 			InsertTeamStage(Team_stage{
-				Championship_id:championship_id,
 				Stage_id:stage.Id,
 				Team_id:team.Id,
 			})
@@ -85,10 +97,118 @@ func GenerateFaseGrupos(teams []Team, championship_id string) (mapB []byte) {
 	return
 }
 
-func GetNewStage() Stage {
+
+func PlayARound(Championship_id string) (mapB []byte) {
+	var listTeamStage []ListTeamStage
+	levelChampionship := GetLevelChampionship(Championship_id)
+
+	json.Unmarshal(GetListStageFromTeam(Championship_id), & listTeamStage)
+
+	for _, element := range listTeamStage{
+		combinations := CombinationsTeam(element.Team)
+		Play(combinations[levelChampionship], element.Id, Championship_id)
+		Play(combinations[5 - levelChampionship], element.Id, Championship_id)
+		UpdateStage(element.Id, 1)
+	}
+
+	mapB, _ = json.Marshal("Todo bien....")
+
+	return
+
+}
+
+func Play(combination Combinations, Stage_id string, Championship_id string) {
+	rand.Seed(time.Now().UnixNano())
+
+	var match Match
+	goalEquipo1 := rand.Intn(5)
+	goalEquipo2 := rand.Intn(5)
+
+	match_id := tools.ChampionshipToken(3)
+	log.Println(goalEquipo1, goalEquipo2)
+	if goalEquipo1 > goalEquipo2 {
+		json.Unmarshal(InsertMatch(Match{
+			Id:match_id,
+			Team_winner:combination.team1.Id,
+			Team_losser:combination.team2.Id,
+			Stage_id:Stage_id,
+		}),&match)
+	} else if goalEquipo1 < goalEquipo2 {
+		json.Unmarshal(InsertMatch(Match{
+			Id:match_id,
+			Team_winner:combination.team2.Id,
+			Team_losser:combination.team1.Id,
+			Stage_id:Stage_id,
+		}),&match)
+	}else {
+		json.Unmarshal(InsertMatch(Match{
+			Id:match_id,
+			Stage_id:Stage_id,
+		}),&match)
+	}
+
+	for index:=0 ; index < goalEquipo1; index++ {
+		player_id := GetIdplayerRamdonDB(combination.team1.Id, Championship_id )
+		goal := Goal{
+			Match_id:match.Id,
+			Player_id:player_id,
+		}
+		if IsExistPlayerGoal(goal){
+			UpdateGoal(goal)
+		}else {
+			goal.Number_goals = 1
+			InsertGoal(goal)
+		}
+
+
+	}
+	for index:=0 ; index < goalEquipo2; index++ {
+		player_id := GetIdplayerRamdonDB(combination.team2.Id, Championship_id )
+		goal := Goal{
+			Match_id:match.Id,
+			Player_id:player_id,
+		}
+		if IsExistPlayerGoal(goal){
+			UpdateGoal(goal)
+		}else {
+			goal.Number_goals = 1
+			InsertGoal(goal)
+		}
+	}
+
+}
+
+func GetListStageFromTeam(Championship_id string) (mapB []byte) {
+	var stages []Stage
+	var listTeamStage []ListTeamStage
+	json.Unmarshal(GetStageFromChampionship(Championship_id), &stages)
+
+	for _, stage := range stages{
+		var team_stage_list ListTeamStage
+		json.Unmarshal(GetTeamFromStage(stage.Id),&team_stage_list)
+		listTeamStage = append(listTeamStage, team_stage_list)
+	}
+	mapB, _ = json.Marshal(listTeamStage)
+	return
+}
+
+func GetStageFromChampionship(Championship_id string) (mapB []byte) {
+	var stage []Stage
+
+	ORM().QueryTable("stage").
+		Filter("championship_id", Championship_id).
+		All(&stage)
+	log.Println("estamos aqui")
+	mapB, _ = json.Marshal(stage)
+
+	return
+}
+
+func GetNewStage(Championship_id string) Stage {
 	return Stage{
 		Id:tools.ChampionshipToken(3),
 		Round:0,
-		IsStage:false,
+		IsStage:0,
+		Championship_id:Championship_id,
 	}
 }

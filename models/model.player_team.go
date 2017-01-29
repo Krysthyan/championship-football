392ca9556bfa6d20389a46bb666cd12a2884337b
krysthyan <krysthyan_09h@yahoo.com>
2017-01-28 23:02:03 -0500

    metodo para obtener ids de jugadores aleatorios

--- models/model.player_team.go
+++ models/model.player_team.go
@@ -2,6 +2,9 @@ package models
 
 import (
 	"encoding/json"
+	"github.com/astaxie/beego/orm"
+	"log"
+	"reflect"
 )
 
 type Player_team struct {
@@ -33,4 +36,35 @@ func GetPlayersFromTeam(id_team, id_championship string) (mapB []byte) {
 	mapB, _ = json.Marshal(players)
 
 	return
+}
+
+func GetIdplayerRamdonDB(Team_id string, Championship_id string) (player_id string) {
+	var listValues []orm.ParamsList
+	qb, _ := orm.NewQueryBuilder("mysql")
+	qb.Select("*").From("player_team").
+		Where("Team_id = ? and Championship_id = ? ").
+		OrderBy("RAND()").Limit(1)
+	ORM().Raw(qb.String(),Team_id, Championship_id).ValuesList(&listValues)
+
+	player_id = listValues[0][0].(string)
+
+	return
+}
+
+func AssingPlayersTeams(team Team, Championship_id string, numAssingPlayers int) {
+	var return_orm interface{}
+
+	for numAssingPlayers > 0 {
+		player_team := Player_team{
+			Player_id:GetPlayerRamdonDB().Id,
+			Championship_id:Championship_id,
+			Team_id:team.Id,
+		}
+
+		json.Unmarshal(InsertPlayerTeam(player_team), &return_orm)
+
+		if reflect.ValueOf(return_orm).Len() > 1 {
+			numAssingPlayers--
+		}
+	}
 }

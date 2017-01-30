9c51669bc7450954f8fa54fcd0d70a3cbe2d4a4e
krysthyan <krysthyan_09h@yahoo.com>
2017-01-30 04:54:20 -0500

    agregado metodo para obtener tabla de posiciones

--- models/model.match.go
+++ models/model.match.go
@@ -1,11 +1,22 @@
 package models
 
+import (
+	"encoding/json"
+	"github.com/astaxie/beego/orm"
+)
 
 type Match struct {
 	Id string `json:"id" orm:"pk"`
 	Team_winner string `json:"team_winner" orm:"null;on_update(set_null)"`
 	Team_losser string `json:"team_losser" orm:"null;on_update(set_null)"`
+	Is_draw int `json:"is_draw"`
 	Stage_id string `json:"stage_id"`
+}
+
+type Posicion struct {
+	Stage_id string `json:"stage_id" orm:"pk"`
+	Name string `json:"name"`
+	Points string `json:"points"`
 }
 
 type Combinations struct {
@@ -29,5 +40,26 @@ func CombinationsTeam(iterable []Team) (partidos []Combinations)  {
 
 		}
 	}
+	return
+}
+
+func GetTablePosition(id_stage string) (mapB []byte)  {
+
+	var position []Posicion
+	var listValues []orm.ParamsList
+
+	qb, _ := orm.NewQueryBuilder("mysql")
+	qb.Select("*").From("posicion").Where("Stage_id = ? ")
+	ORM().Raw(qb.String(), id_stage).ValuesList(&listValues)
+
+	for _, element := range listValues {
+		position = append(position, Posicion{
+			Stage_id:element[0].(string),
+			Name: element[1].(string),
+			Points:   element[3].(string),
+		})
+	}
+	mapB, _ = json.Marshal(position)
+
 	return
 }
